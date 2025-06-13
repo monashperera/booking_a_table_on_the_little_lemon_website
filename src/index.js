2c0f4ccdbce91e6aeabf1d148c21b877685af048
Monash Perera <32927852+monashperera@users.noreply.github.com>
2025-06-13 23:08:09 +0530

    hashrouter use

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './assets/css/fonts.css';
 import './assets/css/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 reportWebVitals();
+
+// <BrowserRouter basename={process.env.PUBLIC_URL}> this for Github pages Deploy
